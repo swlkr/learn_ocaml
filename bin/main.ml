051e11debcb8b532d462632535b4df20a4ce24c9
Sean Walker <swlkr.rbl@gmail.com>
2023-09-07 13:27:19 -0600

    Initial commit

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,25 @@
-let () = print_endline "Hello, World!"
+open Base;;
+
+List.map ~f:(fun x -> x * 2) [ 1; 2; 3 ]
+
+let is_zero x =
+  match x with
+  | 0 -> true
+  | _ -> false
+;;
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/" (fun _ ->
+      Dream.html (hello "world"));
+  ]
+
+Dream.run (Dream.logger (Dream.router [Dream.get "/" ...]))
+
+let hello who =
+  <html>
+  <body>
+    <h1>Hello, <%s who %>!
+(* let () = print_endline string_of_int inc_int 1 *)
